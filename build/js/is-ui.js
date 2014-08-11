279b1005853c5c5683b740b05a26fc8e32dd35a5
Chase Willden <chase.willden@gmail.com>
2014-08-11 13:24:27 -0600

    scrollspy

--- build/js/is-ui.js
+++ build/js/is-ui.js
@@ -67,3 +67,36 @@ $(window).click(function(e){
 		}
 	}
 })
+$(document).ready(function(){
+	var lastId;
+	var scrollMenuItems = $('.nav-right').find('a');
+	var topMenu = $('.nav-top');
+	var topMenuHeight = topMenu.outerHeight() + 15;
+	var scrollItems = scrollMenuItems.map(function(){
+		var item = $($(this).attr("href"));
+	    if (item.length) { return item; }
+	});
+	$(window).scroll(function(){
+		var fromTop = $(this).scrollTop() + topMenuHeight;
+	   
+	   	// Get id of current scroll item
+	   	var cur = scrollItems.map(function(){
+	    	if ($(this).offset().top < fromTop)
+	       	return this;
+	   	});
+	   	if (cur.parent().css('display') === 'none'){
+	   		cur.parent().css({display: 'block'});
+	   	}
+	   	// Get the id of the current element
+	   	cur = cur[cur.length-1];
+	   	var id = cur && cur.length ? cur[0].id : "";
+	   
+	   	if (lastId !== id) {
+	    	lastId = id;
+	       	// Set/remove active class
+	       	scrollMenuItems
+	        .parent().removeClass("active")
+	        .end().filter("[href=#"+id+"]").parent().addClass("active");
+	   	}  
+	})
+})
