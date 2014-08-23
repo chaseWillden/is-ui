884837032f62ecb1c2fdc0aa65a2355137fa2199
= <=>
2014-08-22 19:06:31 -0600

    Fixed the mobile drop down menu

--- build/js/is-ui.js
+++ build/js/is-ui.js
@@ -97,9 +97,12 @@ function lightboxInit(target){
 	}
 }
 function mobileClickInit(target){
-	if (mobilecheck()){
-		//
-		
+	if (mobilecheck() || window.innerWidth < 768){
+		var dropdown = target.parent().children('.dropdown');
+		if (dropdown.length > 0){
+			dropdown.toggleClass('stay-open');
+			dropdown.find('.dropdown').removeClass('stay-open');
+		}
 	}
 }
 function modalInit(target){
